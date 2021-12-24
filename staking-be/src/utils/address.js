8bf90806b3dbbf638e52d2f6e30acfff4abeb9dd
javascript-coding-fan <wdeveloping9099@gmail.com>
2021-12-24 16:12:35 -0500

    config contract address for owner

--- staking-be/src/utils/address.js
+++ staking-be/src/utils/address.js
@@ -54,10 +54,10 @@ export const ContractAddress = {
     busd: ''
   },
   polygonTestnet: {
-    staking: '0x1eddf31d392896f2e7175517edd8f96e405c27f0',
     mmcCrossETH: '0xf82d839a01F5Ab312172c3be2cF5b41a5f906E25',
-    mmcCrossMatic: '0xe4046577e7cc88ba8806d6b62f5a7ba05fd2fe69',
-    rewardERC20: '0x4CFBF140a872a86D81ed68E396c6C33CD5729078'
+    rewardERC20: '0xc3c12cf5424fce9ced9983cac0f3d0ba3fabb3b5',
+    mmcCrossMatic: '0x0e72fd132cf3338d94ed265a88c18b170df828e9',
+    staking: '0xab5384465ec09e66d704027e0b62bbb27d707691',
   },
 
   polygonMainnet: {
--- staking-contract/scripts/deploy_staking.js
+++ staking-contract/scripts/deploy_staking.js
@@ -2,7 +2,7 @@ const { ethers, upgrades } = require("hardhat");
 
 async function main() {
 	const StakeInstance = await ethers.getContractFactory("MmcStaking");
-	const StakeContract = await StakeInstance.deploy('0x4CFBF140a872a86D81ed68E396c6C33CD5729078', '0xe4046577e7cc88ba8806d6b62f5a7ba05fd2fe69');
+	const StakeContract = await StakeInstance.deploy('0xc3c12cf5424fce9ced9983cac0f3d0ba3fabb3b5', '0xe4046577e7cc88ba8806d6b62f5a7ba05fd2fe69');
 	console.log("Staking Contract is deployed to:", StakeContract.address);
 }
 
